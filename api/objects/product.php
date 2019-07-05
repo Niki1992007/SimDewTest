da79a745572e558a61ea2d014c16e497ee82fac7
Nikolay Kvasov <kvasov1992@inbox.ru>
2019-07-05 09:47:07 +0000

    Удалить продукт по ID

--- api/objects/product.php
+++ api/objects/product.php
@@ -153,4 +153,24 @@ class Product
 
         return false;
     }
+
+    // Удалить продукт
+    function delete(){
+
+        // запрос по ID
+        $query = "DELETE FROM " . $this->table_name . " WHERE id = ?";
+
+        $stmt = $this->conn->prepare($query);
+
+        $this->id=htmlspecialchars(strip_tags($this->id));
+
+        $stmt->bindParam(1, $this->id);
+
+        if($stmt->execute()){
+            return true;
+        }
+
+        return false;
+
+    }
 }
